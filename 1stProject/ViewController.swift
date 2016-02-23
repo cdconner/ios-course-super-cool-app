652efd159ecfc6aa693d88b6731b3bb92f310df1
Christopher Conner <Conner@christophersmbp>
2016-02-22 21:34:31 -0600

    Initial Commit

---
+++ 1stProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1stProject
+//
+//  Created by Christopher Conner on 2/22/16.
+//  Copyright © 2016 Christopher Conner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
